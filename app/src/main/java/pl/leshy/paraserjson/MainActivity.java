71400d6927f89eaa7387dd64558e7bdb428096b1
Mateusz Sowiński <Sowa@MacBook-Pro-Mateusz.local>
2017-03-18 15:28:24 +0100

    Initial commit

---
+++ app/src/main/java/pl/leshy/paraserjson/MainActivity.java
@@ -0,0 +1,112 @@
+package pl.leshy.paraserjson;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+import android.widget.SearchView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+
+
+/**
+ * Created by Sowa on 31.01.2017.
+ */
+
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+
+    ArrayList<Product> arrayList = new ArrayList<Product>();
+    ListView list;
+    ListAdapter adapter;
+    SearchView searchField;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = (ListView) findViewById(R.id.listView);
+        searchField = (SearchView) findViewById(R.id.search);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                new ReadJSON().execute("http://www.alticennik.cba.pl/alticennik/cennik.json");
+            }
+        });
+
+        searchField.setOnQueryTextListener(this);
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String query) {
+        adapter.getFilter().filter(query);
+        return false;
+    }
+
+    class ReadJSON extends AsyncTask<String, Integer, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            return readURL(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(String content) {
+            try {
+                JSONObject jsonObject = new JSONObject(content);
+                JSONArray jsonArray =  jsonObject.getJSONArray("cennik");
+
+                for(int i =0;i<jsonArray.length(); i++){
+                    JSONObject productObject = jsonArray.getJSONObject(i);
+                    arrayList.add(new Product(
+                                    productObject.getString("nazwa"),
+                                    productObject.getString("netto"),
+                                    productObject.getString("brutto"),
+                                    productObject.getString("EAN")
+                            )
+                    );
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            adapter = new ListAdapter(
+                    getApplicationContext(), R.layout.list_layout, arrayList
+            );
+            list.setAdapter(adapter);
+        }
+    }
+
+
+    private static String readURL(String theUrl) {
+        StringBuilder content = new StringBuilder();
+        try {
+            URL url = new URL(theUrl);
+            URLConnection urlConnection = url.openConnection();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                content.append(line + "\n");
+            }
+            bufferedReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return content.toString();
+    }
+
+}
